7c6f3ad504c4fa3b45e03ada289ea3ec895652a4
Akshaya <138713862+21R01A0524@users.noreply.github.com>
2025-07-19 17:17:14 +0530

    bitwise_oper.py

--- bitwise_oper.py
+++ bitwise_oper.py
@@ -7,3 +7,33 @@ print(~b)
 print(a^b)
 print(a<<b)
 print(a>>b)
+
+Truth Tables
+AND
+A   B   A&B
+0   0   0
+0   1   0
+1   0   0
+1   1   1
+
+OR
+A   B   A|B
+0   0   0
+0   1   1
+1   0   1
+1   1   1
+  
+XOR
+A   B   A^B
+0   0   0
+0   1   1
+1   0   1
+1   1   0
+
+NOT
+A  ~A
+0   1
+1   0
+
+LEFT SHIFT(<<2)[*2]
+RIGHT SHIFT(>>2)[/2]
